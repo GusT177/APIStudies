9407685d6856eb3667ca3eb10591d5ca5a73f19c
Gustavo <gustavocarmanhanis@gmail.com>
2024-03-14 15:24:14 -0300

    add get infected by id

--- main.go
+++ main.go
@@ -20,6 +20,9 @@ var infecteds = []infected{
   {ID: "2", Name: "boomer", Skill: "Pilk", Skill2: "Scratch"},
   {ID: "3", Name: "smoker", Skill: "Tongue Squeeze", Skill2: "Scratch"},
   {ID: "4", Name: "tank", Skill: "Throw Rocks", Skill2: "Super punch"},
+  {ID: "5", Name: "jockey", Skill: "Ride a survivor", Skill2: "Scratch"}, 
+  {ID: "6", Name: "charger", Skill: "Arm Charge", Skill2: "punch"},
+
 
 }
 
@@ -38,10 +41,22 @@ func PostInfecteds(c *gin.Context){
 
 }
 
+func GetInfectedByID(c *gin.Context){
+    id := c.Param("id")
+  
+    for _, a := range infecteds {
+        if a.ID == id{
+          c.IndentedJSON(http.StatusOK, a)
+          return
+        }
+    }
+  c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Infected not found"})
+}
 
 func main(){
   router := gin.Default()
   router.GET("/infected", GetInfecteds)
+  router.GET("/infected/:id", GetInfectedByID)
   router.POST("/infected", PostInfecteds)
   router.Run("localhost:8080")
 }
